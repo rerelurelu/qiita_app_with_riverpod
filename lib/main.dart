7d81bbc8fc0fccdbe74704ac5368de1f005b9d3f
zoniha <awaawapurin@gmail.com>
2021-06-19 12:35:53 +0900

    🎉 init: Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Qiita App',
+      theme: ThemeData.dark(),
+      home: null,
+    );
+  }
+}
